4b60133fdb78f2c732da9c0fffd8f1138efd0b45
g4l0p3r1d0l <utockakra916@gmail.com>
2026-05-25 09:25:53 +0300

    Add comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+// Основная функция программы
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
+    std::string name;                           // Переменная для имени
+    std::cout << "Enter your name: ";           // Запрос имени
+    std::cin >> name;                           // Ввод имени
     std::cout << "Hello world from " << name << "!" << std::endl;
     return 0;
 }
